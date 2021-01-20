1984a6d23fa7dafc95d36820c5176ecd2a242a79
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2021-01-21 02:57:06 +0300

    done

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
-// Copyright 2020 A.SHT
+// Copyright 2021 ArinaMonicheva
 #include <iostream>
 #include <string>
-
 #include "postfix.h"
 
 int main() {
+  std::string exmpl("(1 + 2) / (3 + 4 * 6.7) - 5.3 * 4.4");
+  std::string exmpl1("2 + 2");
+  std::string exmpl2("2 + 6 * 3 / (4 - 2)");
+  std::string exmpl3("(2 + 8.3) * (6 - 3.2)");
+  std::string exmpl4("(6 * (4 - 2) + 5) * (2.6 + 3 * 7) - 9");
+
+  std::cout << exmpl << "     ->     " << infix2postfix(exmpl) << std::endl;
+  std::cout << exmpl1 << "     ->     " << infix2postfix(exmpl1) << std::endl;
+  std::cout << exmpl2 << "     ->     " << infix2postfix(exmpl2) << std::endl;
+  std::cout << exmpl3 << "     ->     " << infix2postfix(exmpl3) << std::endl;
+  std::cout << exmpl4 << "     ->     " << infix2postfix(exmpl4) << std::endl;
+
   return 0;
 }
--- src/postfix.cpp
+++ src/postfix.cpp
@@ -1,7 +1,64 @@
-// Copyright 2020 A.SHT
+// Copyright 2021 ArinaMonicheva
+#include <string>
+#include <vector>
+#include <sstream>
 #include "MyStack.h"
 #include "postfix.h"
 
 std::string infix2postfix(std::string infix) {
-  return infix;
+  MyStack<char> forOperators(305);  // just for safety (max 100 '(' + all 200 max operators) + safe space
+  bool flag = false;
+  std::string postfixRecord, temp;
+  std::istringstream input(infix);
+
+  while ( input >> temp ) {
+    if ( temp[0] == '(' ) {
+      forOperators.push(temp[0]);
+      temp.erase(0, 1);
+    } else if ( temp.find(')') < temp.length() ) {
+        flag = true;
+        temp.erase(temp.length() - 1, 1);
+    }
+
+    if ( temp[0] > 47 && temp[0] < 58 ) {
+      postfixRecord += temp + ' ';
+    } else if ( temp[0] == '+' || temp[0] == '-') {
+      if ( forOperators.isEmpty() || forOperators.get() == '(' ) {
+        forOperators.push(temp[0]);
+      } else {
+        while ( !(forOperators.isEmpty() || forOperators.get() == '(') ) {
+          postfixRecord += forOperators.pop();
+          postfixRecord += ' ';
+        }
+        forOperators.push(temp[0]);
+      }
+    } else if (temp[0] == '*' || temp[0] == '/') {
+        if ( !(forOperators.get() == '*' || forOperators.get() == '/') ) {
+          forOperators.push(temp[0]);
+        } else {
+          while ( !forOperators.isEmpty() && (forOperators.get() == '/' || forOperators.get() == '*') ) {
+            postfixRecord += forOperators.pop();
+            postfixRecord += ' ';
+          }
+          forOperators.push(temp[0]);
+        }
+    }
+
+    if (flag) {
+      while (forOperators.get() != '(') {
+        postfixRecord += forOperators.pop();
+        postfixRecord += ' ';
+      }
+      forOperators.pop();
+	  flag = false;
+    }
+    
+  }
+
+  while ( !forOperators.isEmpty() ) {
+    postfixRecord += forOperators.pop();
+    postfixRecord += ' ';
+  }
+
+  return postfixRecord;
 }
