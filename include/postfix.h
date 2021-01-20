a5540b27c5a80de31117f08606dc3c2e9f90d483
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2021-01-21 02:56:37 +0300

    done

--- include/postfix.h
+++ include/postfix.h
@@ -1,8 +1,10 @@
-// Copyright 2020 A.SHT
+// Copyright 2021 ArinaMonicheva
 #ifndef INCLUDE_POSTFIX_H_
 #define INCLUDE_POSTFIX_H_
+
 #include <string>
 
 std::string infix2postfix(std::string infix);
 
 #endif  // INCLUDE_POSTFIX_H_
+
